6ffacf059534d67294e7f4aa0e8cdd3d0882db73
Junda Yin <junda.yin1@gmail.com>
2025-03-24 09:45:55 -0500

    changed prod to ts-node .

--- index.ts
+++ index.ts
@@ -1,13 +1,35 @@
 import express from 'express';
+import { readStringFromFile, writeToFile } from './tools';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+let access_count = 0;
+app.get('/', (_, res) => {
   console.log('running');
   res.send({
     hello: 'world'
-  })
+  });
+  writeToFile('./out.txt', access_count++);
+});
+
+app.get('/count', (_, res) => {
+  try {
+    const count = readStringFromFile('./out.txt');
+    res.send({
+      count: count.trim()
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.send({
+        error: 'something went wrong '+err.message
+      });
+    } else {
+      res.send({
+        error: 'something went wrong, no error available.'
+      });
+    }
+  }
 });
 
 app.listen(PORT, () => {
